1487fd44ae366cd67386202a9130991926bcc93f
Aayush0511 <aayushchowdhury.official@gmail.com>
2024-07-30 19:46:59 +0530

    Changed the file upload message

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import seaborn as sns
 
 st.sidebar.title("Whatsapp Chat Analyzer")
 
-uploaded_file = st.sidebar.file_uploader("Choose a file")
+uploaded_file = st.sidebar.file_uploader("Choose a WhatsApp chat file in 24-hour format")
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     data = bytes_data.decode("utf-8")
